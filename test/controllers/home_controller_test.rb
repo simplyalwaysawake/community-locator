19c000a4b540d6cee2b862082f5cb7bb9ccf2c20
Jeremy Haberman <jphaberman@gmail.com>
2024-03-13 16:04:30 -0500

    Update  home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,18 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  include Devise::Test::IntegrationHelpers
+
+  test "shows sign up and sign in links when not logged in" do
+    get home_index_url
+    assert_select "a", "Don't have an account? Sign up"
+    assert_select "a", "Already have an account? Sign in"
+  end
+
+  test "shows sign out link when signed in" do
+    result = sign_in users(:test1)
     get home_index_url
     assert_response :success
+    assert_select "a", "Sign out"
   end
 end
