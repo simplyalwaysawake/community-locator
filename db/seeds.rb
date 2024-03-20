d60a5b2dfdd6d06b87d2bd18d6834fc7a88e666c
Jeremy Haberman <jphaberman@gmail.com>
2024-03-20 07:21:53 -0400

    Seed review database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,22 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+john_doe = User.where(email: 'john.doe@example.com').first_or_create do |user|
+  user.password = 'password123'
+  user.name = 'John Doe'
+  user.telegram = '@johndoe'
+  user.confirmation_sent_at = Time.zone.now
+  user.confirmed_at = Time.zone.now
+
+  user.location = Location.new(
+  {
+    city: 'Minneapolis',
+    state: 'MN',
+    country: 'United States',
+    postal_code: '55401'
+  }
+)
+end
+
+john_doe.save!
