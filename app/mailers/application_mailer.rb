dc3a826be39f311b315fdf72aebaa9c39e50f055
Jeremy Haberman <jeremy@haberman.dev>
2024-04-14 10:29:35 -0500

    Add logging around email sends

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'Simply Always Awake Community Locator <communitylocator@simplyalwaysawake.com>'
   layout 'bootstrap-mailer'
+
+  after_deliver :log_email
+
+  private
+
+  def log_email
+    Rails.logger.info("Email sent to #{mail.to} with subject '#{mail.subject}'")
+  end
 end
