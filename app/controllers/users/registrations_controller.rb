2433ca7dd4cd91ad7169c77925ca6ce8c671a69d
Jeremy Haberman <jeremy@haberman.dev>
2024-06-29 13:19:05 -0500

    Add reCAPTCHA v2 to registration form
    Last week, we started getting a bunch of registration attempts from email addresses that looked suspicious and weren't confirmed.
    
    I chose v2 (instead of v3) because it's simpler and easier to implement.
    
    I set up the reCAPTCHA sites in a new Google Cloud Platform project using the communitylocator@simplyalwaysawake.com user. There are separate keys for dev and staging/prod.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,6 +5,8 @@ module Users
     before_action :configure_sign_up_params, only: [:create] # rubocop:disable Rails/LexicallyScopedActionFilter
     before_action :configure_account_update_params, only: [:update]
 
+    prepend_before_action :check_captcha, only: [:create] # rubocop:disable Rails/LexicallyScopedActionFilter
+
     def edit
       if params['welcome']
         render :welcome
@@ -46,6 +48,17 @@ module Users
 
     def after_inactive_sign_up_path_for(resource) # rubocop:disable Lint/UnusedMethodArgument
       new_user_session_path
+    end
+
+    private
+
+    def check_captcha
+      return if verify_recaptcha
+
+      self.resource = resource_class.new user_params
+      resource.validate
+
+      redirect_to new_user_registration_path, alert: 'reCAPTCHA verification failed'
     end
   end
 end
